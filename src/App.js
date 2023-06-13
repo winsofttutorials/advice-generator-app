67bac1ece49c7cca3c7e78f3a16b34ede4816cea
winsofttutorials <winsofttutorials@gmail.com>
2023-06-13 13:31:25 +0100

    'UIDesign'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
+import AdviceApp from "./components/AdviceApp/AdviceApp";
 
+const API_URL = "https://api.adviceslip.com/advice";
+const addv1 = {
+  id: 132,
+  advice: "Repeat people's names when you meet them.",
+};
 const App = () => {
+  const [advices, setAdvice] = useState([]);
+
+  const myAdvice = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setAdvice(data.slip.advice);
+  };
+
+  useEffect(() => {
+    myAdvice("Repeat people's names when you meet them.");
+  }, [advices]);
+
   return (
     <>
-      <h1>Hello</h1>
+      <div className="container">
+        {advices.forEach((advice) => {
+          <AdviceApp advice={advice} />;
+        })}
+      </div>
     </>
   );
 };
